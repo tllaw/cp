a2544c200f841705f5cb03ac3aae60567968cfc9
tllaw <coder.tllaw@gmail.com>
2022-03-02 10:34:27 +0000

    [+] a-very-big-sum.cpp

---
+++ algorithm/a-very-big-sum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long n, a, r = 0;
+  cin >> n;
+
+  while(n--) {
+    cin >> a;
+    r += a;
+  }
+
+  cout << r << endl;
+  return 0;
+}
