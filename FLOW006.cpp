bd24fa94ea315b4311fbb83166963ce07b9d9438
tllaw <coder.tllaw@gmail.com>
2022-03-06 15:29:53 +0800

    [+] FLOW006.cpp

---
+++ FLOW006.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long t, n;
+  cin >> t;
+
+  while(t--) {
+    long long r = 0;
+    cin >> n;
+
+    while(n) {
+      r += n % 10;
+      n /= 10;
+    }
+
+    cout << r << endl;
+  }
+
+  return 0;
+}
