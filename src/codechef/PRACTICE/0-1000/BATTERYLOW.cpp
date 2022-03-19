a0fcde29419b3e07011e6dd39f96349130158fe4
tllaw <coder.tllaw@gmail.com>
2022-03-19 22:12:43 +0800

    [+] BATTERYLOW.cpp

---
+++ src/codechef/PRACTICE/0-1000/BATTERYLOW.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long t, x;
+  cin >> t;
+
+  while(t--) {
+    cin >> x;
+    cout << (x <= 15 ? "YES" : "NO") << endl;
+  }
+
+  return 0;
+}
