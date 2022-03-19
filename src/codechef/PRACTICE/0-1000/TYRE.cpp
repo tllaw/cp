d509d84043cee104cce9904704149a134ca981b3
tllaw <coder.tllaw@gmail.com>
2022-03-19 22:12:43 +0800

    [+] TYRE.cpp

---
+++ src/codechef/PRACTICE/0-1000/TYRE.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long t, n, m;
+  cin >> t;
+
+  while(t--) {
+    cin >> n >> m;
+    cout << (n * 2 + m * 4) << endl;
+  }
+
+  return 0;
+}
