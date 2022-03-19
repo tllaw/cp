e7695484b78f797f8408b1b8adb8bfdb9da5cfa5
tllaw <coder.tllaw@gmail.com>
2022-03-19 22:12:43 +0800

    [+] JASSIGNMENTS.cpp

---
+++ src/codechef/PRACTICE/0-1000/JASSIGNMENTS.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long t, x;
+  cin >> t;
+
+  while(t--) {
+    cin >> x;
+    cout << (x > 7 ? "NO" : "YES") << endl;
+  }
+
+  return 0;
+}
