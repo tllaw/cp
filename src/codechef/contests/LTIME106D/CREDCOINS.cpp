ac4df26c4aff9e889647e7bfc9d58f4ae6591df3
tllaw <coder.tllaw@gmail.com>
2022-03-20 19:01:51 +0800

    [+] CREDCOINS.cpp

---
+++ src/codechef/contests/LTIME106D/CREDCOINS.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long t, x, y;
+  cin >> t;
+
+  while(t--) {
+    cin >> x >> y;
+    cout << (x * y / 100) << endl;
+  }
+
+  return 0;
+}
