63dda44585ce75e24150ca97c181666243adc1f3
tllaw <coder.tllaw@gmail.com>
2022-03-20 19:01:32 +0800

    [+] BATH.cpp

---
+++ src/codechef/contests/MARCH221D/BATH.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long t, x, y;
+  cin >> t;
+
+  while(t--) {
+    cin >> x >> y;
+    cout << (x / (2 * y)) << endl;
+  }
+
+  return 0;
+}
